325b85ffb772ee3b79c6780bf87680e83577cb47
Draygom <draygom@gmail.com>
2023-05-22 23:02:50 -0300

    implementa o login(sign in)

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const authRouter = express.Router();
 
@@ -25,6 +26,29 @@ authRouter.post("/api/signup", async (req, res) => {
     });
     user = await user.save();
     res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+//sign in
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "Não existe nenhum usuário com esse email" });
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Email ou senha incorretos" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "passWordKey");
+    res.json({ token, ...user._doc });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
